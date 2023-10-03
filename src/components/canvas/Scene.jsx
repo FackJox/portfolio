31b2fbfabf298559da5ff44abcbb7742eceee743
FackJox <jackfox@tuta.io>
2023-10-03 19:01:09 +0100

    head spring

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -1,32 +1,73 @@
-import React from 'react';
+import React, { useTransition, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Stage, PresentationControls, OrbitControls, RandomizedLight, AdaptiveDpr, Environment, AccumulativeShadows, Float } from '@react-three/drei';
+import { Stage, PresentationControls, OrbitControls, OrthographicCamera, AdaptiveDpr, Environment, AccumulativeShadows, Float, View, Center } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 import Head from './Head'
 import Background from '../canvas/Background'
 import { suspend } from 'suspend-react';
-const warehouse = import('@pmndrs/assets/hdri/warehouse.exr')
+import useRefs from 'react-use-refs'
+import warehouse from '@pmndrs/assets/hdri/warehouse.exr'
 
 export default function Scene() {
-  // console.log("warehouse", warehouse)
+  const [ref, view1, view2] = useRefs()
+  // const [warehouse, setWarehouse] = useState(null);
+  // const [isPending, startWarehouseTransition] = useTransition();
+
+  // useEffect(() => {
+  //   startWarehouseTransition(() => {
+  //     setWarehouse(suspend(import('@pmndrs/assets/hdri/warehouse.exr')).default);
+  //   });
+  // }, [startWarehouseTransition]);
+
   return (
-    <Canvas  mode='concurrent' >
+
+    <div ref={ref} className="container">
+    <div className="view1" ref={view1} />
+    <div className="view2" ref={view2} />
+    <Canvas eventSource={ref} className="canvas"
+       dpr={window.devicePixelRatio}
+       onCreated={({ gl }) => {
+         gl.setClearColor(0xFF0000, 0);
+         gl.autoClear = false;
+         gl.clearDepth()
+       }}
+       gl={{ antialias: true, alpha: true }}
+       >
+      <View index={1} track={view1}>
       <AdaptiveDpr pixelated />
-      <Stage intensity={0.5} environment={null}>
 
 
-        <Background />
-        <PresentationControls>
+        <PresentationControls
+        config={{ mass: 2, tension: 500 }}
+        snap={{ mass: 4, tension: 1500 }}
+        rotation={[0, 0.3, 0]}
+        polar={[-Math.PI / 3, Math.PI / 3]}
+        azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+        >
           <Float>
+     
+       <Center >
 
-        <Head />
+        <Head  scale={0.03} />
+       </Center>
           </Float>
-        <OrbitControls />
+        {/* <OrbitControls /> */}
         </PresentationControls>
         {/* <RandomizedLight amount={4} frames={20} position={[5, 5, 10]} intensity={0.1}/> */}
-      <Environment files={suspend(warehouse).default} />
-      </Stage> 
+        <Environment files={warehouse} />
       <Perf />
+      </View>
+
+
+
+      <View index={2} track={view2}>
+          {/* <color attach="background" args={['#d6edf3']} /> */}
+          <OrthographicCamera makeDefault position={[0, 0, 5]} zoom={80} />
+        
+        <Background />
+        </View>
     </Canvas>
+    </div>
+    
   );
 }
